a20888b4237117682c96896d8fb732df2b6236a1
Julio Briones <julesc00@protonmail.com>
2025-05-11 18:21:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Learning FastAPI Course
+
+## Running the application
+To run the FastAPI application, you can use the following command:
+
+```bash
+uvicorn main:app --reload   
+uvicorn main:app --host 0.0.0.0 –-reload
+
+
+```
+
+### Ways to run the application serer
+1. **Using Uvicorn**                                                                                                                                                                                                                                      
---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/employee/{name}/branch/{branch_id}")
+async def get_employee(
+        branch_id:int = Path(..., ge=1),
+        name:str=Path(..., min_length=10),
+        br_name:str=Query(None, min_length=5, max_length=10),
+        age:Optional[int] = None
+):
+    """Use ... (the Ellipsis) as the default inside Path(...) to mark it as required"""
+    return {"name": name, "branch": br_name, "branch_id": branch_id, "age": age}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
